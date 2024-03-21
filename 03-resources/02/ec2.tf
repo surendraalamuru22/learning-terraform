41455769d2c06a4fbb6b83693f6d17e2e0d61e8d
surendra <surendraredddyalamuru01@gmail.com>
2024-03-21 18:53:11 +0530

    resources

--- 03-resources/02/ec2.tf
+++ 03-resources/02/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "sample" {
   ami = "ami-0f3c7d07486cad139"
   instance_type = "t3.micro"
-  security_groups = [aws_security_group.allow_ssh.id]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 }
