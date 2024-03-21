056d185d9d03f3557a01e4fcb6be0dcab5d3fb94
surendra <surendraredddyalamuru01@gmail.com>
2024-03-21 12:07:38 +0530

    variable

--- 02-variables/var.tf
+++ 02-variables/var.tf
@@ -9,3 +9,19 @@ output "sample" {
 output "sample2" {
   value = "Hey, ${var.sample}"
 }
+
+variable "sample2" {
+  default = "hey you"
+}
+
+variable "sample3" {
+  default = 100
+}
+
+variable "sample4" {
+  default = true
+}
+
+output "sample3" {
+  value = var.sample3
+}
