8a90a65482a2d8c6511861c658ef87333f96b96e
surendra <surendraredddyalamuru01@gmail.com>
2024-04-02 10:55:24 +0530

    resources

--- 04-remote-state/ec2.tf
+++ 04-remote-state/ec2.tf
@@ -2,4 +2,8 @@ resource "aws_instance" "sample" {
   ami = "ami-0f3c7d07486cad139"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = Upper(var.tagName)
+  }
 }
