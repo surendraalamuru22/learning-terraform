b8a18dc0252285b781f58e9d23ceb1dca968dbb8
surendra <surendraredddyalamuru01@gmail.com>
2024-04-02 18:09:09 +0530

    loops

--- 05-functions/main.tf
+++ 05-functions/main.tf
@@ -25,3 +25,40 @@ variable "sample1" {
 output "sample1" {
   value = lookup(lookup(var.sample1, "apple", null), "count", 0)
 }
+
+variable "list" {
+  default = ["a", "b", "c"]
+}
+
+output "list" {
+  value = [for s in var.list : upper(s)]
+}
+
+variable "map" {
+  default = {
+    b = "blue"
+    g = "green"
+    y = "yellow"
+  }
+}
+
+output "map" {
+  value = [for k, v in var.map : "Color Code - ${k} | Color - ${v}"]
+}
+
+variable "map1" {
+  default = {
+    c1 = {
+      color_code = "b"
+      color      = "blue"
+    }
+    c2 = {
+      color_code = "g"
+      color      = "green"
+    }
+  }
+}
+
+output "map1" {
+  value = [for k, v in var.map1 : "Color Code - ${v.color_code} | Color - ${v.color}"]
+}
