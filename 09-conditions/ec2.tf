a4eb08a3d4cee5df8646dd9cdb6c24702291c431
surendra <surendraredddyalamuru01@gmail.com>
2024-04-02 18:48:26 +0530

    conditions

---
+++ 09-conditions/ec2.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "sample" {
+  ami = "ami-0f3c7d07486cad139"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = aws_security_group.allow_ssh.*.id
+
+  tags = {
+    Name = "test"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  count = var.create_sg ? 1 : 0
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0b36b10ede3747251"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+variable "create_sg" {
+  default = false
+}
