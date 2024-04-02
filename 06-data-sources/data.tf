36d24c9f7c3ec4e4859aa65e430803ace5bc54a0
surendra <surendraredddyalamuru01@gmail.com>
2024-04-02 12:00:38 +0530

    data source

--- 06-data-sources/data.tf
+++ 06-data-sources/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "example" {
   most_recent      = true
   name_regex       = "centos -8-devops-practice"
-  owners           = ["309956199498"]
+  owners           = ["233035487920"]
 }
 
 output "ami" {
