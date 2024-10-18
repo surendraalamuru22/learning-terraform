30d1699c1ff20b3ecca07c38effc862c725ef938
surendra <surendraredddyalamuru01@gmail.com>
2024-10-18 22:59:33 +0530

    frontend istnce

--- 03-resources/roboshop_instances/instances.tf
+++ 03-resources/roboshop_instances/instances.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "frontend" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.small"
@@ -7,6 +11,93 @@ resource "aws_instance" "frontend" {
   }
 }
 
-provider "aws" {
-  region = "us-east-1"
+resource "aws_instance" "mongodb" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = "mongodb"
+  }
 }
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_instance" "cart" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = "cart"
+  }
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_instance" "user" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_instance" "payment" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_instance" "dispatch" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = "dispatch"
+  }
+}
+
