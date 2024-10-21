9acd82fc93a8adee00eef51f728d899018a1e06c
surendra <surendraredddyalamuru01@gmail.com>
2024-10-21 12:41:51 +0530

    frontend istnce

--- variables/main.tf
+++ variables/main.tf
@@ -9,3 +9,44 @@ output "url" {
 output "url2" {
   value = "URL - ${var.url}"
 }
+
+variable "num1" {
+  default = 10
+}
+
+variable "bool1" {
+  default = True
+}
+
+variable "str1" {
+  default = "HELLO"
+}
+
+variable "str2" {
+  default = "Hello world"
+}
+
+variable "list1" {
+  default = [
+    10,
+  True,
+  "XYZ"
+  ]
+}
+
+variable "map1" {
+  default = {
+    aws = {
+      trainer = "raghu"
+      duration = 20
+    }
+    devops= {
+      trainer = "surendra"
+      duration = 30
+    }
+  }
+}
+
+output "list1" {
+  value = var.list1[3]
+}
