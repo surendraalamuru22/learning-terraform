5c610d77d1f7edbce54d304282a162cc0e90b965
surendra <surendraredddyalamuru01@gmail.com>
2024-10-22 09:12:21 +0530

    frontend istnce

---
+++ loops/roboshop-loop/02/for_each.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "instance" {
+  for_each = var.instances
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = each.key
+  }
+}
+
+variable "instances" {
+  default = {
+    frontend = {}
+    cart = {}
+    catalogue = {}
+    user = {}
+    shipping = {}
+    payment = {}
+    mysql = {}
+    mongodb = {}
+    rabbitmq = {}
+    redis = {}
+  }
+}
