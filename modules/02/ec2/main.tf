839994a6b222b0321714ac9440322043f848e7bc
surendra <surendraredddyalamuru01@gmail.com>
2024-10-22 11:18:43 +0530

    frontend istnce

--- modules/02/ec2/main.tf
+++ modules/02/ec2/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "sample" {
   ami = "ami-0f3c7d07486cad139"
   instance_type = "t3.micro"
-  vpc_security_group_ids = var.SG_ID
+  vpc_security_group_ids = [var.SG_ID]
 }
 
 variable "SG_ID" {}
