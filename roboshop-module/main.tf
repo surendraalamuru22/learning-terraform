10ac15c52967a752a286f5dca4c02efbab014d86
surendra <surendraredddyalamuru01@gmail.com>
2024-10-22 12:13:15 +0530

    frontend istnce

--- roboshop-module/main.tf
+++ roboshop-module/main.tf
@@ -1,20 +1,4 @@
 module "roboshop" {
-  for_each = var.instances
   source = "./module"
-  component_name = each.key
 }
 
-variable "instances" {
-  default = {
-    frontend = {}
-    cart = {}
-    catalogue = {}
-    user = {}
-    shipping = {}
-    payment = {}
-    mysql = {}
-    mongodb = {}
-    rabbitmq = {}
-    redis = {}
-  }
-}
--- roboshop-module/module/main.tf
+++ roboshop-module/module/main.tf
@@ -3,12 +3,26 @@ provider "aws" {
 }
 
 resource "aws_instance" "instances" {
+  for_each = var.instances
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t3.small"
   vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
   tags = {
-    Name = var.component_name
+    Name = each.key
   }
 }
 
-variable "component_name" {}
+variable "instances" {
+  default = {
+    frontend = {}
+    cart = {}
+    catalogue = {}
+    user = {}
+    shipping = {}
+    payment = {}
+    mysql = {}
+    mongodb = {}
+    rabbitmq = {}
+    redis = {}
+  }
+}
