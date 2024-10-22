920e509844ee8249705621949a71c733a07ab7d7
surendra <surendraredddyalamuru01@gmail.com>
2024-10-22 12:02:55 +0530

    frontend istnce

---
+++ roboshop-module/main.tf
@@ -0,0 +1,20 @@
+module "module" {
+  for_each = var.instances
+  source = "./module"
+  component_name = each.key
+}
+
+variable "instances" {
+  default = {
+    frontend = {}
+    cart = {}
+    catalogue = {}
+    user = {}
+    shipping = {}
+    payment = {}
+    mysql = {}
+    mongodb = {}
+    rabbitmq = {}
+    redis = {}
+  }
+}
---
+++ roboshop-module/module/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "instances" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t3.small"
+  vpc_security_group_ids = ["sg-0e2e80b655837f7c6"]
+  tags = {
+    Name = var.component_name
+  }
+}
+
+variable "component_name" {}
