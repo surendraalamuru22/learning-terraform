80fd9f51abb62fe79e6f3416cd2c75c10875268a
surendra <surendraredddyalamuru01@gmail.com>
2024-10-22 11:13:50 +0530

    frontend istnce

--- modules/02/ec2/main.tf
+++ modules/02/ec2/main.tf
@@ -1,3 +1,8 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "sample" {
   ami = "ami-0f3c7d07486cad139"
   instance_type = "t3.micro"
--- modules/02/sg/main.tf
+++ modules/02/sg/main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow TLS inbound traffic"
