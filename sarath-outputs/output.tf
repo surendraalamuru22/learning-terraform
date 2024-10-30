d12d67acdbd659fe6c74b5a1e7c6b730a9bb169e
surendra <surendraredddyalamuru01@gmail.com>
2024-10-30 08:06:49 +0530

    test output

--- sarath-outputs/output.tf
+++ sarath-outputs/output.tf
@@ -1,3 +1,12 @@
 output "test" {
   value = "hello world"
 }
+
+resource "aws_instance" "sarath" {
+  ami = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
