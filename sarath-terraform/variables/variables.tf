8ee58f52a189222375aaeb533900bf7fcc2f0bad
surendra <surendraredddyalamuru01@gmail.com>
2024-10-30 08:50:12 +0530

    variable

--- sarath-terraform/variables/variables.tf
+++ sarath-terraform/variables/variables.tf
@@ -9,3 +9,16 @@ output "sample" {
 output "sample2" {
   value = "hi, ${var.sample}"
 }
+
+
+variable "list" {
+  default = [
+   "test",
+    100,
+    boolean
+  ]
+}
+
+output "sample3" {
+  value = var.list[1]
+}
