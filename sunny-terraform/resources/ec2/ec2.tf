a6f7dd7b71b2579a7ea8249261955efabf7038da
surendra <surendraredddyalamuru01@gmail.com>
2024-11-02 12:09:32 +0530

    variable

--- sunny-terraform/resources/ec2/ec2.tf
+++ sunny-terraform/resources/ec2/ec2.tf
@@ -3,8 +3,9 @@ resource "aws_instance" "sample" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t3.micro"
 
+
   tags = {
-    Name = "dev[count.index]"
+    Name = "sample-${count.index}"
   }
 }
 
